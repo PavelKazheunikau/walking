5785768d68f14e255ff89e8cca2eb73db19e13fa
PavelK25 <kojevnikof@gmail.com>
2026-04-10 07:18:19 +0300

    new search templates

--- myfiles.py
+++ myfiles.py
@@ -7,39 +7,50 @@ Image.MAX_IMAGE_PIXELS = None
 
 def count_items(file_name:str): # defines count of print copies
 
-    start_index  = file_name.find('шт')
+    start_index  = file_name.find('(д)')
     if start_index > -1:
-        while not file_name[start_index-1].isdigit():  
+        while not file_name[start_index-1].isdigit():
             newfilename=file_name[:start_index-1]+file_name[start_index:]    #removing simbols between nums and шт.
             start_index-=1   #go from right to left
         count_num=[]  #number of copies
-        i=1        
+        i=1
         while file_name[start_index-i].isdigit():
             count_num.append(file_name[start_index-i])  #gather digits of num in reverse
-            i+=1            
+            i+=1
         return int(''.join(count_num[::-1]))    #unreverse
     else:
         return 1
 
 search_templates={
-    'solvent':
-    [('с_кл','counter cat promo','counter', 'popup','pop-up','pop_up','fold-up','баннер','промо','скл','podium'),0],
-    'sublimation':
-    [('jc', 'textile', 'press wall cat','габардин', 'мокрый шелк','сатен','моготекс', 'атлас',
-      'бумага', 'оксфорд','дюспо','шоппер','fabric frame','текстиль', 'cветовой', 'space', 'микрофибра' ),0],
-    'direct':
-    [('сетка','прямая'),0]
-    }
+    'p-SOL': {
+        {'m-BNb':['Баннер Блекаут',0]},
+        {'m-BN':['Баннер стандартный',0]},
+        {'m-PP':['Полипропилен',0]}
+        },
+    'p-PRM': {
+        {'m-ISH':['Искусственный шёлк',0]},
+        {'m-FS115':['Флажная сетка 115',0]},
+        {'m-FS90':['Флажная сетка 90',0]}
+        }
+    } #to do: finish dictionary"
 printed_files_num =0
 other_files=0
 dict_to_csv=[]
+#to do: finish dictionary"
+#to do: обход каталога и сохранение списка печатных файлов"
+#to do: площадь заказа в отд функцию"
+#to do: формир словаря с заказами и площ печати по материалам и видам печати"
+#to do: посчитать сумму печати по матералам и категориям"
+#to do: вывод на экран словаря с данными"
+#to do: запись словаря в файл"
+
 for folder, subfolders, filenames in os.walk('.'):
     for file_name in filenames:
-        extention = os.path.splitext(file_name)[1]  # extention      
+        extention = os.path.splitext(file_name)[1]  # extention
         if extention in ('.jpg', '.tiff', '.tif'):
             printed_files_num+=1
-            path = os.path.join(folder, file_name)   # full name      
-            file_info = {}            
+            path = os.path.join(folder, file_name)   # full name
+            file_info = {}
             try:
                 with Image.open(path) as myfile:
                     exif_data = myfile.getexif()
@@ -48,7 +59,7 @@ for folder, subfolders, filenames in os.walk('.'):
                     else:
                         print(f'file {file_name[:15]}... не имеет информации')
                         continue
-                 
+
                 image_width = myfile.width/file_info['XRes']*2.54/100   #meters
                 image_height = myfile.height/file_info['XRes']*2.54/100
                 count_= count_items(file_name.lower())                  # copies
@@ -56,7 +67,7 @@ for folder, subfolders, filenames in os.walk('.'):
                 find_flag = False                                       # to suspend  more search
                 row_dict=[]                                             # data to write to csv
                 for printer_type in search_templates:                   # every type of print
-                    for template in search_templates[printer_type][0]:  # list of search words            
+                    for template in search_templates[printer_type][0]:  # list of search words
                         if template in file_name.lower():
                             search_templates[printer_type][1]+=image_area_m #  add area to specific printer
                             formatted_output = (
@@ -65,7 +76,7 @@ for folder, subfolders, filenames in os.walk('.'):
                                 f'Высота -{image_height:>5.2f} м   '
                                 f'Кол-во -{count_:>3d} шт.   '
                                 f'Площадь -{image_area_m:>6.2f} м.кв.'
-                            )                                            
+                            )
                             print(formatted_output)
                             dict_to_csv.append([printer_type,template, file_name, image_width, image_height, count_, image_area_m])
                             find_flag = True                            # file is calculated
@@ -79,7 +90,7 @@ for folder, subfolders, filenames in os.walk('.'):
                     search_templates['solvent'][1]+=image_area_m
                     dict_to_csv.append(['solvent','непонятка', file_name, image_width, image_height, count_, image_area_m])
                      # file name doesn't fit any search word
-            
+
             except OSError:
                 print("cannot open", file_name)
         else:
@@ -94,4 +105,3 @@ with open('month_data.csv', mode='w', newline='') as csv_file:
     writer=csv.writer(csv_file)
     writer.writerows(dict_to_csv)
 input('Нажми любую клавишу')
-
